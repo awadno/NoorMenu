1b48bc2d01ce61d03822a553246da567444f9864
awadno <noor@danon.tzafonet.org.il>
2017-09-25 08:21:22 +0300

    Initial commit

---
+++ app/src/main/java/com/awad/noor/noormenu/MainActivity.java
@@ -0,0 +1,141 @@
+package com.awad.noor.noormenu;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener, OnClickListener {
+
+    private Button btnDate;
+    private TextView tvShowDate;
+    private Button btnTime;
+    private TextView tvShowTime;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnDate = (Button) findViewById(R.id.btndate);
+        tvShowDate = (TextView) findViewById(R.id.tvShowDate);
+        tvShowTime = (TextView) findViewById(R.id.tvshowTime);
+        btnTime = (Button) findViewById(R.id.btnTime);
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.mnItmScreen1:
+                Toast.makeText(getBaseContext(), "Hi Screen1", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, Screen2.class);
+                startActivity(intent);
+                break;
+            case R.id.mnItmScreen2:
+                Toast.makeText(getBaseContext(), "Hi Screen2", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, Screen2.class);
+                startActivity(intent);
+                break;
+            case R.id.mnItmScreen3:
+                Toast.makeText(getBaseContext(), "Hi Screen3", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, Screen2.class);
+                startActivity(intent);
+                break;
+            case R.id.mnItmScreen4:
+                Toast.makeText(getBaseContext(), "Hi Screen4", Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, Screen2.class);
+                startActivity(intent);
+                break;
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnDate) {
+            Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    tvShowDate.setText(year + "/" + month + "/" + dayOfMonth);
+
+                }
+            };
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, dateListener, year, month, day);
+            datePickerDialog.show();
+
+
+        }
+        if (v == btnTime) {
+            Calendar c1 = Calendar.getInstance();
+            final int hour = c1.get(Calendar.HOUR);
+            final int minute = c1.get(Calendar.MINUTE);
+
+            TimePickerDialog.OnTimeSetListener timeListener = new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                    TextViewTime.setText(hour + ":" + minute);
+                }
+
+
+            };
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/awad/noor/noormenu/TextViewTime.java
@@ -0,0 +1,13 @@
+package com.awad.noor.noormenu;
+
+/**
+ * Created by user on 9/24/2017.
+ */
+public class TextViewTime {
+
+    private static String text;
+
+    public static void setText(String text) {
+        TextViewTime.text = text;
+    }
+}
